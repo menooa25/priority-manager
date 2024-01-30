95b9b75bdaff580a6955937812ad5bdeaa7e105f
menooa25 <menooa2013@gmail.com>
2024-01-30 19:20:12 +0330

    profile page completed

--- app/profile/Profile.tsx
+++ app/profile/Profile.tsx
@@ -1,12 +1,35 @@
 "use client";
 
-import { signOut } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 
 const Profile = () => {
+  const { data, status } = useSession();
+  const [hideImage, setHideImage] = useState(false);
+  const image = data?.user?.image;
+  const nameOfUser = data?.user?.name;
+  useEffect(() => {
+    setHideImage(false);
+  }, [image]);
   return (
-    <button className="btn " onClick={() => signOut()}>
-      خروج از حساب کاربری
-    </button>
+    <div className="flex flex-col h-full justify-between w-full px-3">
+      {nameOfUser && <p dir="rtl">{data?.user?.name} عزیز خوش آمدید 😊</p>}
+      {image && !hideImage && (
+        <div className="avatar w-full flex justify-center mt-3">
+          <div className="w-24 rounded-full">
+            <img
+              src={image}
+              onError={() => setHideImage(true)}
+              referrerPolicy="no-referrer"
+              alt="user avatar"
+            />
+          </div>
+        </div>
+      )}
+      <button className="btn btn-sm w-full mt-10" onClick={() => signOut()}>
+        خروج از حساب کاربری
+      </button>
+    </div>
   );
 };
 
