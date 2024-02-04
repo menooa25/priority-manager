5cdf36ef7507f6b8607c597f9d4458951fc9dd3c
menooa25 <menooa2013@gmail.com>
2024-02-05 00:57:12 +0330

    error page created

---
+++ app/error.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import NetworkError from "./ui/NetworkError";
+
+const GlobalErroPage = () => {
+  return (
+    <>
+      <NetworkError />
+    </>
+  );
+};
+
+export default GlobalErroPage;
